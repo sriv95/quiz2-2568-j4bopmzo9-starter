d2d7e7e23d053b81848db6ac88ab47d8f5656527
sriv95 <srivichai95@gmail.com>
2025-09-08 10:11:33 +0700

    1. Show price

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Button, Stack, Title, Divider, Container } from "@mantine/core";
+import { useState , useEffect } from "react";
+import { Button, Stack, Title, Divider, Container , Text} from "@mantine/core";
 import { v4 as uuidv4 } from "uuid";
 
 type FoodItem = {
@@ -15,6 +15,39 @@ export default function FoodTracker() {
   const [items, setItems] = useState<FoodItem[]>([]);
   const categories = ["Main Course", "Drink", "Dessert"];
 
+  const [sumTotal, setsumTotal] = useState(0)
+  const [sumMainCourse, setsumMainCourse] = useState(0)
+  const [sumDrink, setSumDrink] = useState(0)
+  const [sumDessert, setSumDessert] = useState(0)
+
+  useEffect(() => {
+    let sumTotal = 0
+    let sumMainCourse = 0
+    let sumDrink = 0
+    let sumDessert = 0
+    items.forEach(i => {
+      sumTotal+=Number(i.price)
+      
+      switch (i.category) {
+        case categories[0]:
+          sumMainCourse += Number(i.price)
+          break
+        case categories[1]:
+          sumDrink += Number(i.price)
+          break
+        case categories[2]:
+          sumDessert += Number(i.price)
+          break
+        default:
+          break;
+      }
+    });
+    setsumTotal(sumTotal)
+    setsumMainCourse(sumMainCourse)
+    setSumDrink(sumDrink)
+    setSumDessert(sumDessert)
+  },[items])
+
   return (
     <Container style={{ maxWidth: 600, margin: "auto", padding: 20 }}>
       <Title order={2} mb="md">
@@ -25,8 +58,12 @@ export default function FoodTracker() {
 
       <Divider my="md" />
       {/* Type additional total cost here. */}
-      <Title order={4}>Total cost: {} Baht</Title>
-      <Stack my="sm">{/* Type additional text here. */}</Stack>
+      <Title order={4}>Total: {sumTotal} Baht</Title>
+      <Stack my="sm">
+        <Text>Main Course: {sumMainCourse} Baht</Text>
+        <Text>Drinks: {sumDrink} Baht</Text>
+        <Text>Dessert: {sumDessert} Baht</Text>
+      </Stack>
 
       <Divider my="md" />
       {/* Type additional card here. */}
